4adfbd8a5a340a3a58cd1de53572affab9967b94
komarkom <d.komarov.n@gmail.com>
2020-02-24 18:17:39 +0300

    add abstract layer

--- src/SearchEngines.Web/Util/SearchEngineServices.cs
+++ src/SearchEngines.Web/Util/SearchEngineServices.cs
@@ -1,13 +1,39 @@
 ﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 using SearchEngines.Web.SearchEngines.Base;
+using SearchEngines.Web.SearchEngines.Bing;
+using SearchEngines.Web.SearchEngines.Google;
+using SearchEngines.Web.SearchEngines.Yandex;
 
 namespace SearchEngines.Web.Util
 {
     /// <summary>
     /// Search engines service
     /// </summary>
-    public class SearchEngineServices
+    public class SearchEngineServices: ISearchEngineServices
     {
+        public SearchEngineServices(IConfiguration configuration)
+        {
+            var yandexSearchOption = configuration
+                .GetSection("YandexSearchOption")
+                .Get<YandexSearchOption>();
+
+            var googleSearchOption = configuration
+                .GetSection("GoogleSearchOption")
+                .Get<GoogleSearchOption>();
+
+            var bingSearchOption = configuration
+                .GetSection("BingSearchOption")
+                .Get<BingSearchOption>();
+
+            SearchEngines = new List<ISearchEngine>()
+            {
+                new GoogleSearchEngine(googleSearchOption),
+                new YandexSearchEngine(yandexSearchOption),
+                new BingSearchEngine(bingSearchOption)
+            };
+        }
+
         /// <summary>
         /// Collection of implementation of search engine
         /// </summary>
